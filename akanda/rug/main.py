a977cbe2b3cb0eec64d91ecbc996f9cb8b48c2c0
Rosario Di Somma <rosario.disomma@dreamhost.com>
2013-08-28 22:58:11 +0200

    Purge the mgt tap interface on startup and shutdown
    DHC-1062
    
    Make the rug purge the mgt tap interface on startup and
    shutdown to avoid the "missing tap device" failure when
    the machine the rug is running on is rebooted
    
    Change-Id: Ic056c0db184bd9169bf39d3c62f5b408aded0a36
    Signed-off-by: Rosario Di Somma <rosario.disomma@dreamhost.com>

--- akanda/rug/main.py
+++ akanda/rug/main.py
@@ -8,6 +8,7 @@ from oslo.config import cfg
 from akanda.rug import notifications
 from akanda.rug import scheduler
 from akanda.rug import worker
+from akanda.rug.api import quantum as quantum_api
 
 LOG = logging.getLogger(__name__)
 
@@ -29,7 +30,33 @@ def main(argv=sys.argv[1:]):
         cfg.StrOpt('host',
                    default=socket.getfqdn(),
                    help="The hostname Akanda is running on"),
+
+        # Note(rods): This options, togheter with the AGENT_OPTIONS
+        #             below, were definded in the manager module.
+        #             Since that module is not imported anymore we need
+        #             to define them somewhere else. Adding them here
+        #             temporarely while we figure out what's the best
+        #             to put them
+        cfg.StrOpt('admin_user'),
+        cfg.StrOpt('admin_password', secret=True),
+        cfg.StrOpt('admin_tenant_name'),
+        cfg.StrOpt('auth_url'),
+        cfg.StrOpt('auth_strategy', default='keystone'),
+        cfg.StrOpt('auth_region'),
+
+        # needed for plugging locally into management network
+        cfg.StrOpt('interface_driver'),
+        cfg.StrOpt('ovs_integration_bridge', default='br-int'),
+        cfg.BoolOpt('ovs_use_veth', default=False),
+
     ])
+
+    AGENT_OPTIONS = [
+        cfg.StrOpt('root_helper', default='sudo'),
+    ]
+
+    cfg.CONF.register_opts(AGENT_OPTIONS, 'AGENT')
+
     # FIXME: Convert these to regular options, not command line options.
     cfg.CONF.register_cli_opts([
         cfg.IntOpt('health-check-period',
@@ -44,10 +71,15 @@ def main(argv=sys.argv[1:]):
                    help='connection for AMQP server'),
     ])
     cfg.CONF(argv, project='akanda')
+
     logging.basicConfig(
         level=logging.DEBUG,
         format='%(processName)s:%(name)s:%(levelname)s:%(message)s',
     )
+
+    # Purge the mgt tap interface on startup
+    quantum = quantum_api.Quantum(cfg.CONF)
+    quantum.purge_management_interface()
 
     # Set up the queue to move messages between the eventlet-based
     # listening process and the scheduler.
@@ -80,5 +112,8 @@ def main(argv=sys.argv[1:]):
 
     # Terminate the listening process
     notification_proc.terminate()
+
+    # Purge the mgt tap interface
+    quantum.purge_management_interface()
 
     LOG.info('exiting')
